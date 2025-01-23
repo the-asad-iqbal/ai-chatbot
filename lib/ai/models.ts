4d5d944ef731916a0fe4e551e37603f11d1dc6cb
M Asad Rizvi <asadraoof023@gmail.com>
2025-01-23 18:19:26 +0500

    fix: error fixes in user-profile component

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -7,34 +7,34 @@ export interface Model {
 }
 
 export const models: Array<Model> = [
-  {
-    id: 'Claude 3.5 Sonnet',
-    label: 'Claude 3.5 Sonnet',
-    apiIdentifier: 'claude-3-5-sonnet-20241022',
-    provider: 'Anthropic',
-    isBeta: false
-  },
-  {
-    id: 'Claude 3.5 Haiku',
-    label: 'Claude 3.5 Haiku',
-    apiIdentifier: 'claude-3-5-haiku-20241022',
-    provider: 'Anthropic',
-    isBeta: false
-  },
-  {
-    id: 'gpt-4o-2024-11-20',
-    label: '4o',
-    apiIdentifier: 'gpt-4o-2024-11-20',
-    provider: 'OpenAI',
-    isBeta: false
-  },
-  {
-    id: 'gpt-4o-mini-2024-07-18',
-    label: '4o Mini',
-    apiIdentifier: 'gpt-4o-mini-2024-07-18',
-    provider: 'OpenAI',
-    isBeta: false
-  },
+  // {
+  //   id: 'Claude 3.5 Sonnet',
+  //   label: 'Claude 3.5 Sonnet',
+  //   apiIdentifier: 'claude-3-5-sonnet-20241022',
+  //   provider: 'Anthropic',
+  //   isBeta: false
+  // },
+  // {
+  //   id: 'Claude 3.5 Haiku',
+  //   label: 'Claude 3.5 Haiku',
+  //   apiIdentifier: 'claude-3-5-haiku-20241022',
+  //   provider: 'Anthropic',
+  //   isBeta: false
+  // },
+  // {
+  //   id: 'gpt-4o-2024-11-20',
+  //   label: '4o',
+  //   apiIdentifier: 'gpt-4o-2024-11-20',
+  //   provider: 'OpenAI',
+  //   isBeta: false
+  // },
+  // {
+  //   id: 'gpt-4o-mini-2024-07-18',
+  //   label: '4o Mini',
+  //   apiIdentifier: 'gpt-4o-mini-2024-07-18',
+  //   provider: 'OpenAI',
+  //   isBeta: false
+  // },
   {
     id: 'gemini-2.0-flash-exp',
     label: 'Gemini 2.0 Flash',
